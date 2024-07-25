7f1ec0ec733e246cca2e71c6a5f7b25bff35476c
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-07-25 16:16:41 +0700

    update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,14 +1,31 @@
-(async () => {
-  // rmSync('dist', {
-  //     force: true,
-  //     recursive: true,
-  // });
+import { rmSync } from 'fs';
+import { glob } from 'glob';
 
-  const result = await Bun.build({
-    entrypoints: ['./src/index.ts'],
-    target: 'node',
-    outdir: 'dist',
+(async () => {
+  const startTime = performance.now();
+  console.log('\x1b[32mCleaning dist directory\x1b[0m');
+  rmSync('dist', {
+    force: true,
+    recursive: true,
   });
 
-  console.log(result);
+  console.log('\x1b[34mBuilding dist...\x1b[0m');
+  const entryPoints = await glob('./src/**/*.ts');
+  const result = await Bun.build({
+    entrypoints: entryPoints,
+    target: 'node',
+    outdir: 'dist',
+    external: ['sequelize', 'sqlite3'],
+  });
+
+  const endTime = performance.now();
+  const executionTime = (endTime - startTime) / 1000;
+  if (!result.success) {
+    console.log(`\x1b[31mBuild Failed with execution time ${executionTime.toFixed(2)} s\x1b[0m`);
+    for (const message of result.logs) {
+      console.error(message);
+    }
+  } else {
+    console.log(`\x1b[32mBuild Success with execution time ${executionTime.toFixed(2)} s\x1b[0m`);
+  }
 })();
--- src/services/database.ts
+++ src/services/database.ts
@@ -7,7 +7,6 @@ export default () => {
     storage: 'database.sqlite',
     logging: process.env.NODE_ENV === 'development',
     dialectOptions: {
-      // eslint-disable-next-line no-bitwise
       mode: SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
     },
     define: {
